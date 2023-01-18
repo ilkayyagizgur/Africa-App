091dabd17f6f1c1bbec486b6fb5c5aa59b398345
İlkay Yağız Gür <gurilkayyagiz@gmail.com>
2023-01-18 16:38:58 +0300

    New custom map annotations

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Ada Yagiz Gur on 2.01.2023.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    //Computer property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
