df71da80e69c0b2fc6cb9c4b42e44f897c72577f
İlkay Yağız Gür <gurilkayyagiz@gmail.com>
2023-02-07 01:29:19 +0300

    gallery view added

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    //MARK: - PROPERTIES
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    // SIMPLE GRID DEFINITION
+    // let gridLayout: [GridItem] = [
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible())
+    // ]
+    
+    // EFFICIENT GRID DEFINITION
+    // let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYNAMIC GRID DEFINITION
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
+    //MARK: - BODY
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 30) {
+                //MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.accentColor, lineWidth: 5))
+                
+                //MARK: - SLIDER
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding(.horizontal)
+                .onChange(of: gridColumn, perform: { value in
+                    withAnimation(.easeOut){
+                        gridSwitch()
+                    }
+                })
+                
+                //MARK: - GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                withAnimation(.easeOut(duration: 0.5)) {
+                                    selectedAnimal = item.image
+                                    haptics.impactOccurred()
+                                }
+                            }
+                    }//: FOR EACH
+                }//: GRID
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            }//: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 30)
+        }//: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
